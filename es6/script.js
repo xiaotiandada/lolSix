f6f7eddd6a05f4ab1663f4c0a23888457747382e
xiaotiandada <952822399@qq.com>
2018-03-19 00:54:54 +0800

    Animation effects

--- es6/script.js
+++ es6/script.js
@@ -1,21 +1,43 @@
 
 (function(){
-    let navBtns = $('#nav i')
-    let navBtnli = $('#nav li')
+    let navBtns = $('#nav-ul i')
+    let navBtnli = $('#nav-ul li')
     let numLi = 1
-    let classNames =  'ans2_btn' + numLi
     let navI = null
     let time = null
+    let nametop = null
+
+    let navhs = $('#nav li')
+    let navhem = $('#nav em')
+    let numh = 1
+    let navh = null
+    let timeh = null
+    let nameh = null
 
     navBtnli.on('mouseover',function(){
         navI = $(this).children('i')
-        run()
+        nametop = 'ans2_btn' 
+        run('ans2_btn')
     })
     navBtnli.on('mouseout',function(){
         clearTimeout(time)
-        // navI.removeClass()
         navI.css('display','none')
     })
+
+    navhs.on('mouseover',function(){
+        $(this).removeClass('ligb')
+        navh =  $(this).children('em')
+        nameh = 'ans_btn'
+        runh()
+    })
+
+    navhs.on('mouseout',function(){
+        clearTimeout(timeh)
+        navh.css('display','none')        
+        $(this).children('em').removeClass()        
+        $(this).addClass('ligb')        
+    })
+
 
     function run(){
         if(numLi > 25){
@@ -23,9 +45,20 @@
         }
         navI.css('display','block')        
         navI.removeClass()
-        navI.addClass('ans2_btn' + numLi)
+        navI.addClass(nametop + numLi)
         numLi++
         time = setTimeout(run, 30)
+    }
+
+    function runh(){
+        if(numh > 25){
+            numh = 1
+        }
+        navh.css('display','block')        
+        navh.removeClass()
+        navh.addClass(nameh + numh)
+        numh++
+        timeh = setTimeout(runh, 30)
     }
 
 })()
--- public/js/script.js
+++ public/js/script.js
@@ -1,21 +1,42 @@
 'use strict';
 
 (function () {
-    var navBtns = $('#nav i');
-    var navBtnli = $('#nav li');
+    var navBtns = $('#nav-ul i');
+    var navBtnli = $('#nav-ul li');
     var numLi = 1;
-    var classNames = 'ans2_btn' + numLi;
     var navI = null;
     var time = null;
+    var nametop = null;
+
+    var navhs = $('#nav li');
+    var navhem = $('#nav em');
+    var numh = 1;
+    var navh = null;
+    var timeh = null;
+    var nameh = null;
 
     navBtnli.on('mouseover', function () {
         navI = $(this).children('i');
-        run();
+        nametop = 'ans2_btn';
+        run('ans2_btn');
     });
     navBtnli.on('mouseout', function () {
         clearTimeout(time);
-        // navI.removeClass()
         navI.css('display', 'none');
+    });
+
+    navhs.on('mouseover', function () {
+        $(this).removeClass('ligb');
+        navh = $(this).children('em');
+        nameh = 'ans_btn';
+        runh();
+    });
+
+    navhs.on('mouseout', function () {
+        clearTimeout(timeh);
+        navh.css('display', 'none');
+        $(this).children('em').removeClass();
+        $(this).addClass('ligb');
     });
 
     function run() {
@@ -24,9 +45,20 @@
         }
         navI.css('display', 'block');
         navI.removeClass();
-        navI.addClass('ans2_btn' + numLi);
+        navI.addClass(nametop + numLi);
         numLi++;
         time = setTimeout(run, 30);
+    }
+
+    function runh() {
+        if (numh > 25) {
+            numh = 1;
+        }
+        navh.css('display', 'block');
+        navh.removeClass();
+        navh.addClass(nameh + numh);
+        numh++;
+        timeh = setTimeout(runh, 30);
     }
 })();
 //# sourceMappingURL=script.js.map
